37f8b3b157d5f73bdcf83e6131cc94b1b5d452d1
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-01-24 20:58:51 +0100

    FPS3Version

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    public static float RestrictAngle(float angle, float angleMin, float angleMax)
+    {
+        if (angle > 180)
+            angle -= 360;
+        else if (angle < -100)
+            angle += 360;
+
+        if (angle > angleMax)
+            angle = angleMax;
+        if (angle < angleMin)
+            angle = angleMin;
+
+        return angle;
+    }
+
+
     public Transform head;
-    // Start is called before the first frame update
+
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
@@ -22,7 +38,7 @@ public class PlayerControler : MonoBehaviour
     {
         Vector3 e = head.eulerAngles;
         e.x -= Input.GetAxis("Mouse Y") * 2f;
-        //e.x = RestrictAngle(e.x, -85f, 85f);
+        e.x = RestrictAngle(e.x, -85f, 85f);
         head.eulerAngles = e;
     }
 }
