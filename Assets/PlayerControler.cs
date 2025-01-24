cf75b7d8b0e40393fcb0b44e182bf4275879084c
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-01-24 22:19:13 +0100

    Update PlayerControler.cs

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+<<<<<<< HEAD
     public static float RestrictAngle(float angle, float angleMin, float angleMax) //definit l'angle maximum et l'angle minimum et s'assure qu'ils ne sont pas depasses
     {
         if (angle > 180)
@@ -23,12 +24,17 @@ public class PlayerControler : MonoBehaviour
     public Transform head; //tete du personnage, a attribuer a la cameraa
 
     void Start() //bloque le curseur
+=======
+    public Transform head;
+    // Start is called before the first frame update
+    void Start()
+>>>>>>> parent of 8bf833b (Merge branch 'main' of https://github.com/Esma-LouisBl/Georges)
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-
+    // Update is called once per frame
     void Update()
     {
         Cursor.visible = false;
@@ -39,7 +45,7 @@ public class PlayerControler : MonoBehaviour
     {
         Vector3 e = head.eulerAngles;
         e.x -= Input.GetAxis("Mouse Y") * 2f;
-        e.x = RestrictAngle(e.x, -85f, 85f);
+        //e.x = RestrictAngle(e.x, -85f, 85f);
         head.eulerAngles = e;
     }
 }
