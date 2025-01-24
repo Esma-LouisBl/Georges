94c6379dd529b71f4dea575438b0d7d0e23a5ea5
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-01-24 20:46:49 +0100

    1stFPS_Version

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Transform head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 e = head.eulerAngles;
+        e.x -= Input.GetAxis("Mouse Y") * 2f;
+        e.x = RestrictAngle(e.x, -85f, 85f);
+        head.eulerAngles = e;
+    }
+}
