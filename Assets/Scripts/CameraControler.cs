181e3dafe6b280f79ee645cae12edc1fecf02965
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-01-25 06:23:13 +0100

    CleanOff3

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -23,12 +23,20 @@ public class CameraControler : MonoBehaviour
 
     [SerializeField]
     private GameObject _player;
+    [SerializeField]
+    private GameObject _camera;
+
+    private bool _isWalking = false;
+    private bool _handsUp = false;
 
 
     void Start()
     {
         Cursor.visible = false; //cache le curseur
         Cursor.lockState = CursorLockMode.Locked; //bloque le curseur au centre de l'ecran
+
+        //_isWalking = true;
+        StartCoroutine(Shaking());
     }
 
 
@@ -45,5 +53,26 @@ public class CameraControler : MonoBehaviour
         e.x -= Input.GetAxis("Mouse Y") * 2f; //2f : represente la vitesse a laquelle la camera monte
         e.x = RestrictAngle(e.x, -85f, 85f); //bloque la rotation de la camera
         head.eulerAngles = e; //tourne la camera sur l'axe vertical
+    }
+
+    private IEnumerator Shaking() //rajouter les conditions grounded
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+            if (_isWalking)
+            {
+                if (_handsUp)
+                {
+                    _camera.transform.position += new Vector3(0, 0.1f, 0);
+                    _handsUp = false;
+                }
+                else
+                {
+                    _camera.transform.position += new Vector3(0, -0.1f, 0);
+                    _handsUp = true;
+                }
+            }
+        }
     }
 }
