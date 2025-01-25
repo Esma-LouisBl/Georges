21c4c3b002d388dc75a607a333b20ebeaca6e321
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-01-25 18:10:09 +0100

    commit dans le vent

--- Assets/Scripts/TriggerBridge.cs
+++ Assets/Scripts/TriggerBridge.cs
@@ -12,6 +12,19 @@ public class TriggerBridge : MonoBehaviour
 
     [SerializeField]
     private Rigidbody _rb;
+
+    [SerializeField]
+    private float _speed = 3;
+
+    [SerializeField]
+    private int _lives = 3;
+
+    private bool _goForward = true;
+    public GameObject point1;
+    public GameObject point2;
+
+    [SerializeField]
+    private Mission _mission;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +34,26 @@ public class TriggerBridge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (_goForward)
+        {
+            _rb.velocity = Vector3.forward * _speed;
+        }
+        else
+        {
+            _rb.velocity = Vector3.back * _speed;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject == point1)
+        {
+            _goForward = false;
+        }
+
+        if (collision.gameObject == point2)
+        {
+            _goForward = true;
+        }
         //if (collision.gameObject.CompareTag("Bubble"))
         //{
         //    Debug.Log("skibidi");
