348d01b02da63585dbf29da0f7010a71dd5997b8
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-01-25 17:12:22 +0100

    Jauge de bulles qui marche EZ

--- Assets/Scripts/BubbleSlider.cs
+++ Assets/Scripts/BubbleSlider.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BubbleSlider : MonoBehaviour
 {
-
+    [SerializeField]
+    private PlayerShoot _playerShoot;
+    [SerializeField]
+    private Slider _gauge;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gauge.maxValue = _playerShoot.charger;
+        _gauge.value = _playerShoot.charger;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _gauge.value = _playerShoot.charger;
     }
 }
