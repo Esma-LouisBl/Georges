f7d3c0bb1ba9e0313dd5127e3811cb654fe24ed2
ESMA-JulienP <148343029+CastorHunter@users.noreply.github.com>
2025-01-25 10:20:23 +0100

    Bubbles!

--- Assets/Scripts/ProjectilesBehavior.cs
+++ Assets/Scripts/ProjectilesBehavior.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class ProjectilesBehavior : MonoBehaviour
 {
-    public float Speed = 4.5f;
+    private float _speed = 5f;
+    private float _timeTillEnd = 5;
 
+    private void Start()
+    {
+        StartCoroutine(TimeToSlow());
+    }
     void Update()
     {
-        transform.position += -transform.right * Time.deltaTime * Speed;
+        transform.position += ((-transform.right * Time.deltaTime * _speed) + new Vector3(0,Random.Range(-0.01f,0.01f), Random.Range(-0.01f, 0.01f)));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator TimeToSlow()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            _speed -= 0.25f;
+            _timeTillEnd -= 1;
+            if (_timeTillEnd < 0)
+            {
+                BubbleExplosion();
+            }
+        }
+    }
+
+    private void BubbleExplosion()
     {
         Destroy(gameObject);
     }
