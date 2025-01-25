873e9b283423393da73ddfb26f83c82cdc1e0d02
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-01-25 13:11:05 +0100

    Georges repousse le joueur

--- Assets/Scripts/HitTaken.cs
+++ Assets/Scripts/HitTaken.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class HitTaken : MonoBehaviour
 {
     public GameObject player;
+    private Rigidbody _rb;
 
+    public float knockbackForce = 5f;
+    public float knockbackDuration = 0.2f;
+    private Vector3 knockbackDirection;
+    private float knockbackTimer = 0f;
     public Transform spawner;
 
     private CharacterController characterController;
@@ -18,16 +23,26 @@ public class HitTaken : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (knockbackTimer > 0)
+        {
+            knockbackTimer -= Time.deltaTime;
+            characterController.Move(knockbackDirection * knockbackForce * Time.deltaTime);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
-            characterController.enabled = false;
-            player.transform.position = spawner.transform.position;
-            characterController.enabled = true;
+            knockbackDirection = (player.transform.position - other.transform.position).normalized;
+
+            // Initialise le timer pour appliquer le knockback
+            knockbackTimer = knockbackDuration;
+
+            //characterController.enabled = false;
+            //player.transform.position = spawner.transform.position;
+            //characterController.enabled = true;
         }
     }
 }
