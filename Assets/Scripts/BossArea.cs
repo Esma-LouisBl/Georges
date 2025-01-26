d9b1917c7a29836b35113e6ab44f2827e76c7071
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-01-26 03:36:58 +0100

    UI de Roi qui apparait

---
+++ Assets/Scripts/BossArea.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossArea : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _ui;
+    [SerializeField]
+    private EnemyKing _enemyKing;
+    [SerializeField]
+    private BoxCollider _area;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ui.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_enemyKing.lives < 1)
+        {
+            _ui.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _ui.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _ui.SetActive(false);
+        }
+    }
+}
